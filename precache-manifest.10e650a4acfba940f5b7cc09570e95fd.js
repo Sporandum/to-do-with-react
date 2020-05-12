58cb0fbb7460a7f6e3ddd1914c83d34d057fc166
Sporandum <sporandum@gmail.com>
2020-05-12 19:05:27 +0200

    Updates

---
+++ precache-manifest.10e650a4acfba940f5b7cc09570e95fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a734111621e12a430131d293f2dd4582",
+    "url": "/to-do-with-react/index.html"
+  },
+  {
+    "revision": "deced1758c4e533cf9bc",
+    "url": "/to-do-with-react/static/css/main.09b0d62f.chunk.css"
+  },
+  {
+    "revision": "b8061ac1a2c0b443b3b9",
+    "url": "/to-do-with-react/static/js/2.ad65c074.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-with-react/static/js/2.ad65c074.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deced1758c4e533cf9bc",
+    "url": "/to-do-with-react/static/js/main.34afe1fe.chunk.js"
+  },
+  {
+    "revision": "057533d3d28cd3f2a87e",
+    "url": "/to-do-with-react/static/js/runtime-main.7733e36d.js"
+  }
+]);
